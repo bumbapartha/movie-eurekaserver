4d2605e34ae851de54e281ff06e57df1c0d388ce
consultant <consultant@192.168.0.105>
2022-01-12 13:29:24 +0530

    More changes

---
+++ src/test/java/com/movie/movieeurekaserver/MovieEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.movieeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
